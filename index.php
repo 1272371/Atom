9d1dfe56912454d4f7bfa9e4ae97d6db1369b3ba
Ntsiki-Nonjo <815108@students.wits.ac.za>
2018-08-16 13:50:37 +0200

    used index.php instead of index.html

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
+<?php
+    /*
+     * if user is authenticated stay on the same page otherwise
+     * redirect to login page 
+     */
+    header("Location: /dist/login");
+?>
 
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+
+    <!--CSS-->
+    <link rel="stylesheet" href="bootstrap/bootstrap.css">
+    <script src="main.js"></script>
+    <!--Title-->
+    <title>@Risk</title>
+</head>
+<body>
+    <!--Scripts-->
+    <script src="jquery/jquery.js"></script>
+    <script src="bootstrap/bootstrap.js"></script>
+    <script src="js/popper.min.js"></script>
+</body>
+</html>
