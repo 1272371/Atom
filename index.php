3077f3d3bc28440b3dd3e308d9598a233296cc90
Ntsiki-Nonjo <815108@students.wits.ac.za>
2018-08-20 15:52:05 +0200

    file structure

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
 <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/home.css" />
     <link rel="icon" type="image/png" href="img/favicon.png">
 
     <!--CSS-->
-    <link rel="stylesheet" href="bootstrap/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
     <!--Title & Icon-->
     <title>
         Welcome
@@ -118,8 +118,8 @@
     </div>
 
     <!--Scripts-->
-    <script src="jquery/jquery.js"></script>
-    <script src="bootstrap/bootstrap.js"></script>
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.js"></script>
     <script src="js/popper.min.js"></script>
 </body>
 </html>
