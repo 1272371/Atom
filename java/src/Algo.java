df9ab13d0643fa81530bd44ce19eb892a625ee14
Suleiman <1190016@students.wits.ac.za>
2018-08-21 21:05:27 +0200

    java

---
+++ java/src/Algo.java
@@ -0,0 +1,16 @@
+/*
+	A dummy file for the algorithm to be implemented later
+*/
+
+public class Algo {
+
+    public static void main(String[]args){
+        algo();
+    }
+    private static int algo(){
+        int a = 0;
+        if (Math.random() < 0.5)a = 0;
+        else a = 1;
+        return a;
+    }
+}
