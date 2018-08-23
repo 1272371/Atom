430e12c303de9bf1d499b5d0243cba8cc58fbd40
1272371 <1272371@users.noreply.github.com>
2018-08-23 04:18:53 +0200

    first test for login

---
+++ test/php.php
@@ -0,0 +1,16 @@
+/**
+ * PHPUnit test class for \Maths\Fibonacci class.
+ */
+class FibonacciTest extends \PHPUnit_Framework_TestCase
+{
+    /** Test login success */   
+    public function TestLogin()
+    {
+        $this->assertEquals(1, Login(1234567,password));
+    }
+      /** Test login fail */  
+     public function TestLogin()
+    {
+        $this->assertEquals(0,Login(1234565,password));
+    }
+}
