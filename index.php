27d08baa3fac15ba0b2385f6bd5440588fa58a56
Ntsiki-Nonjo <815108@students.wits.ac.za>
2018-08-26 16:51:40 +0200

    cleaned up login

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<?php include_once 'php/header.php'?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -22,8 +21,42 @@
     </title>
 </head>
 <body>
+    <header>
+        <!-- Navigation -->
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img class="img-fluid" src="img/asset/wits-logo.png" style="width: 96px; height: 57px;"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item ml-2 mr-2 active">
+                            <a class="nav-link" href="#">Home</a>
+                        </li>
+                        <li class="nav-item ml-2 mr-2">
+                            <a class="nav-link" href="dist/about/">About</a>
+                        </li>
+                        <li class="nav-item ml-2 mr-4">
+                            <a class="nav-link" href="dist/team/">Team</a>
+                        </li>
+                        <li class="nav-item">
+                            <form id="sign-in-form" action="php/login.php" method="POST">
+                                <input class="form-module ml-2 mr-2" style="width: 10vw" type="text" name="user" placeholder="Username">
+                                <input class="form-module ml-2 mr-2" style="width: 10vw" type="password" name="pass" placeholder="Password">
+                                <button class="btn btn-outline-secondary ml-2 mr-2"
+                                    style="color: #fff" type="submit" name="submit">Sign In</button>
+                            </form>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
     <!-- Jumbotron -->
-    <div class="container-fluid">
+    <div class="container-fluid mt-5">
         <div class="row jumbotron">
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center">
                 <h1 style="font-size:5rem;">teach, learn, collaborate</h1>
@@ -36,57 +69,45 @@
     </div>
 
     <!-- Cards -->
-    <div class="container-fluid padding">
-        <div class="row padding">
-            <div class="col-md-3">
-                <div class="card mx-auto" style="width: 18vw;">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 col-lg-3">
+                <div class="card mx-auto w-75">
                     <img class="card-img-top" src="img/card/view-grades-card.png">
                     <div class="card-body">
-                        <h4 class="card-title"></h4>
-                        <p class="card-text">
-                            In order to pass, you need to keep up with your grades,
-                            check your grades regularly
-                        </p>
-                        <a href="#" class="btn btn-outline-secondary">View grades</a> <!--submit button-->
+                        <div class="text-center">
+                            <a href="#" class="btn btn-primary w-100">Staff Training</a> <!--submit button-->
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-3">
-                <div class="card mx-auto" style="width: 18vw;">
+            <div class="col-md-12 col-lg-3">
+                <div class="card mx-auto w-75">
                     <img class="card-img-top" src="img/card/view-grades-card.png">
                     <div class="card-body">
-                        <h4 class="card-title"></h4>
-                        <p class="card-text">
-                            View overall academic performance with graphs, charts,
-                            and other statistics
-                        </p>
-                        <a href="#" class="btn btn-outline-secondary">Graph overview</a> <!--submit button-->
+                        <div class="text-center">
+                            <a href="#" class="btn btn-danger w-100">Help</a> <!--submit button-->
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-3">
-                <div class="card mx-auto" style="width: 18vw;">
+            <div class="col-md-12 col-lg-3">
+                <div class="card mx-auto w-75">
                     <img class="card-img-top" src="img/card/view-grades-card.png">
                     <div class="card-body">
-                        <h4 class="card-title"></h4>
-                        <p class="card-text">
-                            In order to pass, you need to keep up with your grades,
-                            check your grades regularly
-                        </p>
-                        <a href="#" class="btn btn-outline-secondary">Reports</a> <!--submit button-->
+                        <div class="text-center">
+                            <a href="#" class="btn btn-warning w-100">Getting Started</a> <!--submit button-->
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-3">
-                <div class="card mx-auto" style="width: 18vw;">
+            <div class="col-md-12 col-lg-3">
+                <div class="card mx-auto w-75">
                     <img class="card-img-top" src="img/card/view-grades-card.png">
                     <div class="card-body">
-                        <h4 class="card-title"></h4>
-                        <p class="card-text">
-                            In order to pass, you need to keep up with your grades,
-                            check your grades regularly
-                        </p>
-                        <a href="#" class="btn btn-outline-secondary">Summary</a> <!--submit button-->
+                        <div class="text-center">
+                            <a href="#" class="btn btn-success w-100">About Us</a> <!--submit button-->
+                        </div>
                     </div>
                 </div>
             </div>
