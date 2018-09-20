b472bf79b54061eabb220c3b479e5226682bb686
yanoki <Kyrenreddy@gmail.com>
2018-09-20 18:06:13 +0200

    Testing how to get java to run on the webpage

--- java/src/Algo.java
+++ java/src/Algo.java
@@ -1,17 +1,27 @@
 /*
 	A dummy file for the algorithm to be implemented later
 */
-
-public class Algo {
-
-    public static void main(String[]args){
-        int pass = algo();
+import javax.swing.*;
+import javax.swing.text.html.Option;
+import  java.awt.*;
+import java.applet.*;
+public class Algo extends Applet{
+    public void init(){
 
     }
-    private static int algo(){
+    public void start(){
+        JOptionPane.showMessageDialog(null,"Started");
+    }
+/*    public void algo(){
         int a = 0;
         if (Math.random() < 0.5)a = 0;
         else a = 1;
-        return a;
+        //return a;
+    }*/
+    public void paint(Graphics  g){
+        g.drawOval(0,0,250,100);
+        g.setColor(Color.cyan);
+        g.drawString("testing ",10,50);
+        JOptionPane.showMessageDialog(null,"worked");
     }
 }
