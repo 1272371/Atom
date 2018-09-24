880fcec9518eb31a87d1aa1cea87ba825caeb1ff
Suleiman <1190016@students.wits.ac.za>
2018-09-24 12:40:55 +0200

    made the student details panel of dashboard display live results from database

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -88,37 +88,6 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-md-4" style="max-height: 400px;overflow:scroll">
-                        
-
-
-
-
-                        <div class="card card-user">
-                            <div class="image">
-                                <img src="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&fm=jpg&h=300&q=75&w=400" alt="..."/>
-                            </div>
-                            <div class="content">
-                                <div class="author">
-                                     <a href="#">
-                                    <img class="avatar border-gray" src="assets/img/faces/face-0.jpg" alt="..."/>
-
-                                      <h4 class="title">Phillip Chaney<br />
-                                         <small>2005890</small>
-                                      </h4>
-                                    </a>
-                                </div>
-                                <p class="description text-center"> 
-                                    Science 
-                                    <br>
-                                        BSc Gen 
-                                    <br>
-                                                   
-                                </p>
-                            </div>
-                            <hr>
-                            
-                        </div>
-
                         <?php
 
                                 $link = mysqli_connect("localhost","1234567","password", "api_risk");
