466af0cf999d6a785d5b20a4f16df7dcb1f4e3cf
yanoki <Kyrenreddy@gmail.com>
2018-09-24 17:27:03 +0200

    Working on the java applet

--- java/src/Algo.java
+++ java/src/Algo.java
@@ -1,27 +1,33 @@
 /*
 	A dummy file for the algorithm to be implemented later
 */
+import com.sun.net.httpserver.HttpsServer;
+
 import javax.swing.*;
 import javax.swing.text.html.Option;
+import java.applet.Applet;
 import  java.awt.*;
-import java.applet.*;
-public class Algo extends Applet{
+
+public class Algo extends Applet {
     public void init(){
 
     }
-    public void start(){
-        JOptionPane.showMessageDialog(null,"Started");
+    public void paint (Graphics g) {
+        g.drawString ("Hello World", 25, 50);
     }
-/*    public void algo(){
-        int a = 0;
-        if (Math.random() < 0.5)a = 0;
-        else a = 1;
-        //return a;
-    }*/
-    public void paint(Graphics  g){
-        g.drawOval(0,0,250,100);
-        g.setColor(Color.cyan);
-        g.drawString("testing ",10,50);
-        JOptionPane.showMessageDialog(null,"worked");
-    }
+//    public void start(){
+//        JOptionPane.showMessageDialog(null,"Started");
+//    }
+//    public void algo(){
+//        int a = 0;
+//        if (Math.random() < 0.5)a = 0;
+//        else a = 1;
+//        //return a;
+//    }
+//    public void paint(Graphics  g){
+//        g.drawOval(0,0,250,100);
+//        g.setColor(Color.cyan);
+//        g.drawString("testing ",10,50);
+//        JOptionPane.showMessageDialog(null,"worked");
+//    }
 }
