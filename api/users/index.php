0f21b5c7f3ac524794b5a18bfc3acc938a586c65
Ntsiki-Nonjo <815108@students.wits.ac.za>
2018-09-26 18:11:26 +0200

    try again

--- api/users/index.php
+++ api/users/index.php
@@ -1,3 +1,18 @@
 <?php
 
-    echo $_SERVER['QUERY_STRING'];
+    $query = explode("&", $_SERVER['QUERY_STRING']);
+    $count = count($query);
+
+    if ($count == 1) {
+
+        if (strlen($query[0]) == 0) {
+
+            // no query was passed in
+            header("Location: get.php");
+        }
+        else {
+
+            // redirect to relevant page
+            header("Location: user.php?" . $query[0]);
+        }
+    }
