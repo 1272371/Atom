64b83056aa0c792a4490e21e48900dd4de8dddb6
Yanoki <1233647@students.wits.ac.za>
2018-09-26 10:19:51 +0200

    Add files via upload
    Some test file for the algorithm

---
+++ java/src/Testing_database_access.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Kyren
+ * Date: 2018/09/25
+ * Time: 14:29
+ */
+$servername = "localhost";
+$username = "1234567";
+$password = "password";
+$dbname = "risk";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+$link = mysqli_connect("localhost","1234567","password", "risk");
+if ($link->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
