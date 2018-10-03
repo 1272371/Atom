4c23c908a16fef52f51cf44c75c5a130f115c3a0
Suleiman <1190016@students.wits.ac.za>
2018-10-03 20:59:52 +0200

    fixed class list button on both pages and segmented the php code into functions

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -134,29 +134,7 @@
                                 <ul class="dropdown-menu">
                                 
                                 <?php
-                                    class buttonDropdown{
-
-                                    public function listClasses(){
-
-                                        $link = mysqli_connect("localhost","1234567","password", "api_risk");
-
-                                        if (mysqli_connect_error()){
-                                            die ("Error!");
-                                        }
-
-                                        $query2 = "SELECT module_code FROM module";
-                                        
-                                        $result2= mysqli_query($link, $query2);
-
-                                        while ($row2 = mysqli_fetch_array($result2)) {
-                                            echo "<li><a href='#'>".$row2['module_code']."</a></li>";
-                                        }
-                                    }
-
-                                    }
-
-                                    buttonDropdown::listClasses();
-                                
+                                    Dashboard::listClasses();
                                 ?>
 
                                 </ul>
@@ -167,40 +145,7 @@
                                 <table class="table table-hover">
                                 
                                 <?php
-
-                                $link = mysqli_connect("localhost","root","", "risk");
-
-                                    if (mysqli_connect_error()){
-                                        die ("Error!");
-                                    }
-
-                                    $query = "SELECT * FROM user";
-                                    
-
-                                    echo "<table class='table table-hover'>";
-                                    echo "<thead>";
-                                    echo        "<th>Student Number</th>";
-                                    echo        "<th>Name</th>";
-                                    echo        "<th>Current Average (%)</th>";
-                                    echo "</thead>";
-
-                                    $result = mysqli_query($link, $query);
-                                    
-
-                                    while ($row = mysqli_fetch_array($result)) {
-                                        
-                                        $person=$row['user_id'];
-                                        $query2 = "SELECT ROUND(AVG(mark_total)) FROM mark where user_id=".$person;
-                                        $result2= mysqli_query($link, $query2);
-                                        $row2 = mysqli_fetch_array($result2);
-
-                                        $student = $person;
-                                       
-                                        echo "<tr style='cursor:pointer;' class='clickable-row' data-href='dashboard.php?student=$student'><td>".$row['user_id']."</td><td>".$row['user_name']." ".$row['user_surname']."</td><td>".$row2['ROUND(AVG(mark_total))']."</td></tr>";
-  
-                                }
-                                    echo "</table>";
-                                    
+                                    Dashboard::getStudentList(); 
                                 ?>
 
                             <script>
@@ -242,78 +187,129 @@
 </body>
 
 <?php
-                                class Dashboard{
-                                    public function getLatestMarks(){
+    class Dashboard{
+        public function getLatestMarks(){
 
-                                        $link = mysqli_connect("localhost","root","", "risk");
+            $link = mysqli_connect("localhost","root","", "risk");
 
-                                        if (mysqli_connect_error()){
-                                            die ("Error!");
-                                        }
+            if (mysqli_connect_error()){
+                die ("Error!");
+            }
 
-                                        if (empty($_GET)){
-                                            $student = "500594";
-                                        } else {
-                                            $student = $_GET['student'];
-                                        }
+            if (empty($_GET)){
+                $student = "500594";
+            } else {
+                $student = $_GET['student'];
+            }
 
-                                      
-                                        $query2 = "SELECT mark_total FROM mark WHERE user_id=".$student; 
-                                        $query3 = "SELECT course_name,course_code FROM course";// WHERE grades.module = module.id";
+          
+            $query2 = "SELECT mark_total FROM mark WHERE user_id=".$student; 
+            $query3 = "SELECT course_name,course_code FROM course";// WHERE grades.module = module.id";
 
-                                        echo "<table class='table table-hover'>";
+            echo "<table class='table table-hover'>";
 
-                                        echo "<thead>";
-                                        echo        "<th>Subject</th>";
-                                        echo        "<th>Subject Code</th>";
-                                        echo        "<th>Current Mark (%)</th>";
-                                        echo "</thead>";
+            echo "<thead>";
+            echo        "<th>Subject</th>";
+            echo        "<th>Subject Code</th>";
+            echo        "<th>Current Mark (%)</th>";
+            echo "</thead>";
 
-                                        
-                                        $result2= mysqli_query($link, $query2);
-                                        $result3= mysqli_query($link, $query3);
+            
+            $result2= mysqli_query($link, $query2);
+            $result3= mysqli_query($link, $query3);
 
-                                        while ($row2 = mysqli_fetch_array($result2)) {
-                                            
-                                                if ($row3 = mysqli_fetch_array($result3)){
-                                            
-                                            echo "<tr><td>".$row3['course_name']."</td><td>".$row3['course_code']."</td><td>".$row2['mark_total']."</td></tr>";
-                                            }
-                                        }
-                                        echo "</table>";
-                                    }
+            while ($row2 = mysqli_fetch_array($result2)) {
+                
+                    if ($row3 = mysqli_fetch_array($result3)){
+                
+                echo "<tr><td>".$row3['course_name']."</td><td>".$row3['course_code']."</td><td>".$row2['mark_total']."</td></tr>";
+                }
+            }
+            echo "</table>";
+        }
 
-                                    public function getStudentDetails(){
+        public function getStudentDetails(){
 
-                                        if (empty($_GET)){
-                                            $student = "500594";
-                                        } else {
-                                            $student = $_GET['student'];
-                                        }
+            if (empty($_GET)){
+                $student = "500594";
+            } else {
+                $student = $_GET['student'];
+            }
 
-                                        $link = mysqli_connect("localhost","root","", "risk");
+            $link = mysqli_connect("localhost","root","", "risk");
 
-                                        if (mysqli_connect_error()){
-                                            die ("Error!");
-                                        }
+            if (mysqli_connect_error()){
+                die ("Error!");
+            }
 
-                                        $query = "SELECT * FROM user WHERE user_id=".$student;
-                                        $result = mysqli_query($link, $query);
+            $query = "SELECT * FROM user WHERE user_id=".$student;
+            $result = mysqli_query($link, $query);
 
-                                        if ($row = mysqli_fetch_array($result)) {
-                                        echo "<h4 class='title'>".$row['user_name']." ".$row['user_surname']."<br />";
-                                        echo         "<small>".$row['user_id']."</small>";
-                                        echo      "</h4>";
-                                        echo    "</a>";
-                                        echo "</div>";
-                                        echo "<p class='description text-center'>" ;
-                                        echo    "Science";
-                                        }
-                                    }
-                                }
-                                
-                                
-                            ?>
+            if ($row = mysqli_fetch_array($result)) {
+            echo "<h4 class='title'>".$row['user_name']." ".$row['user_surname']."<br />";
+            echo         "<small>".$row['user_id']."</small>";
+            echo      "</h4>";
+            echo    "</a>";
+            echo "</div>";
+            echo "<p class='description text-center'>" ;
+            echo    "Science";
+            }
+        }
+
+        public function getStudentList(){
+            $link = mysqli_connect("localhost","root","", "risk");
+
+                if (mysqli_connect_error()){
+                    die ("Error!");
+                }
+
+                $query = "SELECT * FROM user";
+                
+
+                echo "<table class='table table-hover'>";
+                echo "<thead>";
+                echo        "<th>Student Number</th>";
+                echo        "<th>Name</th>";
+                echo        "<th>Current Average (%)</th>";
+                echo "</thead>";
+
+                $result = mysqli_query($link, $query);
+                
+
+                while ($row = mysqli_fetch_array($result)) {
+                    
+                    $person=$row['user_id'];
+                    $query2 = "SELECT ROUND(AVG(mark_total)) FROM mark where user_id=".$person;
+                    $result2= mysqli_query($link, $query2);
+                    $row2 = mysqli_fetch_array($result2);
+
+                    $student = $person;
+                   
+                    echo "<tr style='cursor:pointer;' class='clickable-row' data-href='dashboard.php?student=$student'><td>".$row['user_id']."</td><td>".$row['user_name']." ".$row['user_surname']."</td><td>".$row2['ROUND(AVG(mark_total))']."</td></tr>";
+
+            }
+                echo "</table>";
+        }
+
+        public function listClasses(){
+            $link = mysqli_connect("localhost","root","", "risk");
+
+            if (mysqli_connect_error()){
+                die ("Error!");
+            }
+
+            $query2 = "SELECT course_code FROM course";
+            
+            $result2= mysqli_query($link, $query2);
+
+            while ($row2 = mysqli_fetch_array($result2)) {
+                echo "<li><a href='#'>".$row2['course_code']."</a></li>";
+            }
+        }
+    }
+    
+    
+?>
 
     <!--   Core JS Files   -->
     <script src="assets/js/jquery.3.2.1.min.js" type="text/javascript"></script>
--- dashboard/grade.php
+++ dashboard/grade.php
@@ -91,29 +91,8 @@
                 </button>
                 <ul class="dropdown-menu">
                    <?php
-                   class buttonDropdown{
-
-                    public function listClasses(){
-
-                        $link = mysqli_connect("localhost","1234567","password", "api_risk");
-
-                        if (mysqli_connect_error()){
-                            die ("Error!");
-                        }
-
-                        $query2 = "SELECT module_code FROM module";
-                        
-                        $result2= mysqli_query($link, $query2);
-
-                        while ($row2 = mysqli_fetch_array($result2)) {
-                            echo "<li><a href='#'>".$row2['module_code']."</a></li>";
-                        }
-                    }
-                        }
-
-                    buttonDropdown::listClasses();
-                
-                ?>
+                        grade::listClasses();
+                    ?>
                 </ul>
         </div> 
                 </div>
@@ -133,48 +112,8 @@
                             <div class="content table-responsive table-full-width">
                                 <table class="table table-hover">
                                     <?php
-
-                                    $link = mysqli_connect("localhost","root","", "risk");
-
-                                    if (mysqli_connect_error()){
-                                        die ("Error!");
-                                    }
-
-                                    $query = "SELECT * FROM user";
-                                    
-
-                                    echo "<table class='table table-hover'>";
-                                    echo "<thead>";
-                                    echo        "<th>Student Number</th>";
-                                    echo        "<th>Name</th>";
-                                    echo        "<th>Faculty</th>";
-                                    echo        "<th>YOS</th>";
-                                    echo        "<th>Current Average (%)</th>";
-                                    echo "</thead>";
-
-                                    $result = mysqli_query($link, $query);
-                                    
-
-                                    while ($row = mysqli_fetch_array($result)) {
-                                        
-                                        $person=$row['user_id'];
-                                        $query2 = "SELECT ROUND(AVG(mark_total)) FROM mark where user_id=".$person;
-                                        $result2= mysqli_query($link, $query2);
-                                        $row2 = mysqli_fetch_array($result2);
-
-                                        $student = $person;
-                                       
-                                        echo "<tr style='cursor:pointer;' class='clickable-row' data-href='dashboard.php?student=$student'><td>".$row['user_id']."</td><td>".$row['user_name']." ".$row['user_surname']."</td><td>Science</td><td>2</td><td>".$row2['ROUND(AVG(mark_total))']."</td><td>"."<a href='dashboard.php' type='button' class='btn btn-light btn-sm'>More Info</a>"."</td></tr>";
-
-                                        
-											/*echo "<tr style='cursor:pointer;' class='clickable-row' data-href='dashboard.php?student=$student'><td>".$row['student_nr']."</td><td>".$row['user_name']." ".$row['user_surname']."</td><td>".$row['user_coursecode']."</td><td>"."2018 - ".$row['user_enrollmentyear']."</td><td>".$row2['ROUND(AVG(percentage))']."</td><td>"."<a href='dashboard.php' type='button' class='btn btn-light btn-sm'>More Info</a>"."</td></tr>";*/
-                                        
-
-                                   
-                                }
-                                    echo "</table>";
-                                    
-                                ?>
+                                        grade::getStudentList();
+                                    ?>
 
                                 <script>
                                             jQuery(document).ready(function($) {
@@ -193,6 +132,70 @@
         </div>
     </div>
 </div>
+
+<?php
+    class grade{
+        public function getStudentList(){
+            $link = mysqli_connect("localhost","root","", "risk");
+
+                if (mysqli_connect_error()){
+                    die ("Error!");
+                }
+
+                $query = "SELECT * FROM user";
+                
+
+                echo "<table class='table table-hover'>";
+                echo "<thead>";
+                echo        "<th>Student Number</th>";
+                echo        "<th>Name</th>";
+                echo        "<th>Faculty</th>";
+                echo        "<th>YOS</th>";
+                echo        "<th>Current Average (%)</th>";
+                echo "</thead>";
+
+                $result = mysqli_query($link, $query);
+                
+
+                while ($row = mysqli_fetch_array($result)) {
+                    
+                    $person=$row['user_id'];
+                    $query2 = "SELECT ROUND(AVG(mark_total)) FROM mark where user_id=".$person;
+                    $result2= mysqli_query($link, $query2);
+                    $row2 = mysqli_fetch_array($result2);
+
+                    $student = $person;
+                   
+                    echo "<tr style='cursor:pointer;' class='clickable-row' data-href='dashboard.php?student=$student'><td>".$row['user_id']."</td><td>".$row['user_name']." ".$row['user_surname']."</td><td>Science</td><td>2</td><td>".$row2['ROUND(AVG(mark_total))']."</td><td>"."<a href='dashboard.php' type='button' class='btn btn-light btn-sm'>More Info</a>"."</td></tr>";
+
+                    
+                        /*echo "<tr style='cursor:pointer;' class='clickable-row' data-href='dashboard.php?student=$student'><td>".$row['student_nr']."</td><td>".$row['user_name']." ".$row['user_surname']."</td><td>".$row['user_coursecode']."</td><td>"."2018 - ".$row['user_enrollmentyear']."</td><td>".$row2['ROUND(AVG(percentage))']."</td><td>"."<a href='dashboard.php' type='button' class='btn btn-light btn-sm'>More Info</a>"."</td></tr>";*/
+                    
+
+               
+            }
+                echo "</table>";
+
+        }
+
+        public function listClasses(){
+
+            $link = mysqli_connect("localhost","1234567","password", "api_risk");
+
+            if (mysqli_connect_error()){
+                die ("Error!");
+            }
+
+            $query2 = "SELECT module_code FROM module";
+            
+            $result2= mysqli_query($link, $query2);
+
+            while ($row2 = mysqli_fetch_array($result2)) {
+                echo "<li><a href='#'>".$row2['module_code']."</a></li>";
+            }
+        }
+    }
+?>
 
 
 </body>
