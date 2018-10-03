4bef1892c2255b6ca0bbb98a41c8c908b542c46d
Suleiman <1190016@students.wits.ac.za>
2018-10-03 20:31:09 +0200

    connected grade book to risk.sql database

--- dashboard/grade.php
+++ dashboard/grade.php
@@ -134,7 +134,7 @@
                                 <table class="table table-hover">
                                     <?php
 
-                                    $link = mysqli_connect("localhost","1234567","password", "api_risk");
+                                    $link = mysqli_connect("localhost","root","", "risk");
 
                                     if (mysqli_connect_error()){
                                         die ("Error!");
@@ -157,17 +157,17 @@
 
                                     while ($row = mysqli_fetch_array($result)) {
                                         
-                                        $person=$row['student_nr'];
-                                        $query2 = "SELECT ROUND(AVG(percentage)) FROM grades where user_id=".$person;
+                                        $person=$row['user_id'];
+                                        $query2 = "SELECT ROUND(AVG(mark_total)) FROM mark where user_id=".$person;
                                         $result2= mysqli_query($link, $query2);
                                         $row2 = mysqli_fetch_array($result2);
 
                                         $student = $person;
                                        
-                                        /* FOR LIVE DATABASEecho "<tr style='cursor:pointer;' class='clickable-row' data-href='dashboard.php?student=$student'><td>".$row['user_id']."</td><td>".$row['user_name']." ".$row['user_surname']."</td><td>"."<a href='dashboard.php' type='button' class='btn btn-light btn-sm'>More Info</a>"."</td></tr>";*/
+                                        echo "<tr style='cursor:pointer;' class='clickable-row' data-href='dashboard.php?student=$student'><td>".$row['user_id']."</td><td>".$row['user_name']." ".$row['user_surname']."</td><td>Science</td><td>2</td><td>".$row2['ROUND(AVG(mark_total))']."</td><td>"."<a href='dashboard.php' type='button' class='btn btn-light btn-sm'>More Info</a>"."</td></tr>";
 
                                         
-											echo "<tr style='cursor:pointer;' class='clickable-row' data-href='dashboard.php?student=$student'><td>".$row['student_nr']."</td><td>".$row['user_name']." ".$row['user_surname']."</td><td>".$row['user_coursecode']."</td><td>"."2018 - ".$row['user_enrollmentyear']."</td><td>".$row2['ROUND(AVG(percentage))']."</td><td>"."<a href='dashboard.php' type='button' class='btn btn-light btn-sm'>More Info</a>"."</td></tr>";
+											/*echo "<tr style='cursor:pointer;' class='clickable-row' data-href='dashboard.php?student=$student'><td>".$row['student_nr']."</td><td>".$row['user_name']." ".$row['user_surname']."</td><td>".$row['user_coursecode']."</td><td>"."2018 - ".$row['user_enrollmentyear']."</td><td>".$row2['ROUND(AVG(percentage))']."</td><td>"."<a href='dashboard.php' type='button' class='btn btn-light btn-sm'>More Info</a>"."</td></tr>";*/
                                         
 
                                    
