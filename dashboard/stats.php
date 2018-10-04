ed6535e9f791709f4b5b9a9f3e29121f66f3c7d6
Suleiman <1190016@students.wits.ac.za>
2018-10-04 18:46:42 +0200

    added stats page php functions into own file and fixed its class selector

--- dashboard/stats.php
+++ dashboard/stats.php
@@ -91,31 +91,10 @@
                                 </button>
                                 <ul class="dropdown-menu">
 
-
                                 
                                 <?php
-                                class buttonDropdown{
-
-	                             	public function listClasses(){
-
-		                                $link = mysqli_connect("localhost","1234567","password", "api_risk");
-
-		                                if (mysqli_connect_error()){
-		                                    die ("Error!");
-		                                }
-
-		                                $query2 = "SELECT module_code FROM module";
-		                                
-		                                $result2= mysqli_query($link, $query2);
-
-		                                while ($row2 = mysqli_fetch_array($result2)) {
-		                                    echo "<li><a href='#'>".$row2['module_code']."</a></li>";
-		                                }
-	                            	}
-                            }
-
-                            buttonDropdown::listClasses();
-                                
+                                    include_once('statsTest.php');
+                                    echo StatsTesting::listClasses();
                                 ?>
 
                                 </ul>
