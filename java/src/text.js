c386b7bf893b133c112066589e7a2fd6a1d7c3f0
yanoki <Kyrenreddy@gmail.com>
2018-10-05 08:12:22 +0200

    Some testing for uploads

--- java/src/text.js
+++ java/src/text.js
@@ -5,17 +5,38 @@ window.onload = function() {
     fileInput.addEventListener('change', function(e) {
         var file = fileInput.files[0];
         var textType = file.type;
-        alert("doc type"+ file.type);
+
         if (file.type.match(textType)) {
             var reader = new FileReader();
 
             reader.onload = function(e) {
                 fileDisplayArea.innerText = reader.result;
             }
-
             reader.readAsText(file);
         } else {
             fileDisplayArea.innerText = "File not supported!";
         }
     });
+}
+
+function drag_drop(event) {
+    event.preventDefault();
+
+
+
+    var fileDisplayArea = document.getElementById('fileDisplayArea');
+    var file = event.dataTransfer.files[0];
+    var textType = file.type;
+
+    if (file.type.match(textType)) {
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            fileDisplayArea.innerText = reader.result;
+        }
+
+        reader.readAsText(file);
+    } else {
+        fileDisplayArea.innerText = "File not supported!";
+    }
 }
