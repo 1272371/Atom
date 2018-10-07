fb7f4e6ce001e36cc9d651264bf3618704909c0e
Ntsiki-Nonjo <815108@students.wits.ac.za>
2018-10-07 16:05:16 +0200

    IAP marks 2014

--- pop.php
+++ pop.php
@@ -4,7 +4,7 @@
 
     if ($conn) {
 
-        $row = file('csv/COMS1015-BCO-2018.csv', FILE_SKIP_EMPTY_LINES);
+        $row = file('csv/COMS1018-IAP-2014', FILE_SKIP_EMPTY_LINES);
         $length = count($row);
 
         for ($i = 1; $i < $length; $i++) {
@@ -17,15 +17,17 @@
             $mark1 = (double) chop($column[1]);
             $mark2 = (double) chop($column[2]);
             $mark3 = (double) chop($column[3]);
+            $mark4 = (double) chop($column[4]);
 
 
             $query = 'INSERT INTO
             mark
             (mark_total, user_id, assessment_id)
             VALUES
-            (' . $mark1 . ', ' . $id . ', 16),
-            (' . $mark2 . ', ' . $id . ', 17),
-            (' . $mark3 . ', ' . $id . ', 18)';
+            (' . $mark1 . ', ' . $id . ', 19),
+            (' . $mark2 . ', ' . $id . ', 20),
+            (' . $mark3 . ', ' . $id . ', 21),
+            (' . $mark4 . ', ' . $id . ', 22)';
 
             if ($conn->query($query)) {
                 echo 'success ' . $id . ' <br>';
