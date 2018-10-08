b36f422341cd6157aba51f2298014e7afbd8470a
Ntsiki-Nonjo <815108@students.wits.ac.za>
2018-10-08 10:29:23 +0200

    db2

--- api/config/Database.php
+++ api/config/Database.php
@@ -2,15 +2,12 @@
 
     class Database {
 
-        /**
-         * database parameters
-         * live variables
-         */
-        
+        // database parameters
         private $host = 'localhost';
         private $name = 'd815108';
         private $user = 's815108';
         private $pass = 'random123';
+        private $conn;
 
         /**
          * uncomment to connect locally
@@ -21,22 +18,25 @@
         //private $user = 'root';
         //private $pass = '';
 
-        // database connection
-        private $conn;
-
         // connection function
         public function connect() {
+
             $this->conn = null;
+
             try {
                 // uses php database object (PDO)
                 $this->conn = new PDO(
                     'mysql:host=' . $this->host . ';dbname=' . $this->name,
                     $this->user, $this->pass);
+
                 // set attributes
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
             } catch (PDOException $e) {
                 echo 'Connection Error: ' . $e->getMessage();
             }
+
             return $this->conn;
         }
+
     }
