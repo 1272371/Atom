efbea1ff09a738ebf463f44f60750517325d3a6d
Ntsiki-Nonjo <815108@students.wits.ac.za>
2018-10-08 10:23:09 +0200

    db

--- api/config/Database.php
+++ api/config/Database.php
@@ -6,38 +6,24 @@
          * database parameters
          * live variables
          */
-        /*
+        
         private $host = 'localhost';
         private $name = 'd815108';
         private $user = 's815108';
         private $pass = 'random123';
-        */
+
         /**
          * uncomment to connect locally
          * then comment live variables
          */
-         private $host = '127.0.0.1';
-         private $name = 'risk';
-         private $user = 'root';
-         private $pass = '';
+        //private $host = '127.0.0.1';
+        //private $name = 'risk';
+        //private $user = 'root';
+        //private $pass = '';
 
         // database connection
         private $conn;
 
-        public function setHost(string $host): void
-        {
-            $this->host = $host;
-        }
-        public function __construct(string $host , string $name, string $user, string $pass)
-        {
-            if($host!=""){
-                $host=$this->setHost("127.0.0.1");
-            }
-            $this->host = $host;
-            $this->name = $name;
-            $this->user = $user;
-            $this->pass = $pass;
-        }
         // connection function
         public function connect() {
             $this->conn = null;
