1b58725f5836ef6f97139309b39a00cb0afadfe2
Ntsiki-Nonjo <815108@students.wits.ac.za>
2018-10-08 10:57:04 +0200

    db5

--- api/signing/login.php
+++ api/signing/login.php
@@ -34,7 +34,7 @@
         }
         else {
             // couldn't login
-            http_response_code(200);
+            http_response_code(400);
             echo json_encode(array('message' => 'error'));
         }
     }
