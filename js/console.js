56b4b645a8841ae5041eda5f69a810d7525174f0
Ntsiki-Nonjo <815108@students.wits.ac.za>
2018-10-08 15:53:44 +0200

    console

---
+++ js/console.js
@@ -0,0 +1,38 @@
+/**
+ * cosmetics
+ */
+// body and html
+$('body,html').attr('style','overflow-x:hidden;max-height:100vh');
+
+// side navigation
+$('#side-nav-bar').attr('style', 'background-color: #242424;min-height: 100vh; height: 100%; overflow:hidden');
+
+/**
+ * function runs immediately
+ */
+$(document).ready(function() {
+
+    // set title
+    Welcome();
+
+});
+
+/**
+ * functions
+ */
+function Welcome() {
+
+    $.ajax({
+        url : 'api/signing/signed.php', // url
+        method : 'POST', // method
+        dataType : 'JSON',
+        success : function(data) {
+            
+            if (data.message === 'success') {
+                // redirect to dashboard
+                $('#dev-id').html('>>> Signed in as, ' + data.contents.user_name);
+            }
+        }
+    });
+
+}
