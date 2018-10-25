fe25609133184f68065b084e4c9e61302e7055f9
1272371 <1272371@students.wits.ac.za>
2018-10-25 22:54:13 +0000

    48%

--- tests/phpunit/api/models/AssessmentTest.php
+++ tests/phpunit/api/models/AssessmentTest.php
@@ -7,6 +7,7 @@
  * Time: 6:42 PM
  */
 include_once __DIR__ .'../../../../../api/models/Assessment.php';
+include_once __DIR__ .'../../../../../api/config/Database.php';
 
 class AssessmentTest extends PHPUnit\Framework\TestCase
 {
@@ -16,13 +17,20 @@ class AssessmentTest extends PHPUnit\Framework\TestCase
      * Date: 2018/10/03
      * Time: 11:32 PM
      */
+    private $host = '127.0.0.1';
+    private $dbname = 'risk';
+    private $user = 'root';
+    private $user_='admin';
+    private $pass = '';
     private $db = 'risk';
-    private $courseArray = ['e'];
+    private $courseArray = [1,2,3];
     /**
      * @covers Assessment::addAssessment
      */
     public function testAddAssessment()
     {
+        $this->conn = new Database($this->host, $this->dbname, $this->user, $this->pass);
+        $this->db=$this->conn->connect();
         $assessment = new Assessment($this->db);
         $this->assertNotEmpty($assessment->AddAssessment());
     }
@@ -31,7 +39,10 @@ class AssessmentTest extends PHPUnit\Framework\TestCase
      */
     public function testGetPassRate()
     {
-
+        $this->conn = new Database($this->host, $this->dbname, $this->user, $this->pass);
+        $this->db=$this->conn->connect();
+        $assessment = new Assessment($this->db);
+        $this->assertNotEmpty($assessment->getPassRate(1,60));
     }
     /**
      * @covers Assessment::getSummary
@@ -39,6 +50,8 @@ class AssessmentTest extends PHPUnit\Framework\TestCase
 
     public function testGetSummary()
     {
+        $this->conn = new Database($this->host, $this->dbname, $this->user, $this->pass);
+        $this->db=$this->conn->connect();
         $assessment = new Assessment($this->db);
         $this->assertNotEmpty($assessment->getSummary($this->courseArray));
     }
@@ -48,7 +61,22 @@ class AssessmentTest extends PHPUnit\Framework\TestCase
 
     public function testgetMinMaxYear()
     {
+        $this->conn = new Database($this->host, $this->dbname, $this->user, $this->pass);
+        $this->db=$this->conn->connect();
         $assessment = new Assessment($this->db);
-        $this->assertNotEmpty($assessment->getMinMaxYear());
+        $course_id =$assessment->course_id;
+        $this->assertNull($assessment->getMinMaxYear($course_id));
     }
+
+    /**
+     * @covers Assessment::__constructor
+     */
+public function test__construct()
+    {
+        $this->conn = new Database($this->host, $this->dbname, $this->user, $this->pass);
+        $this->db=$this->conn->connect();
+        $assessment = new Assessment($this->db);
+        $this->assertNull($assessment->__construct($this->db));
+    }
+
 }
