96043abbf229681a141de29036d6da504708ef2f
yanoki <Kyrenreddy@gmail.com>
2018-11-24 14:25:45 +0200

    integration of ldap working(should work for both staff and students now)

--- api/signing/login.php
+++ api/signing/login.php
@@ -35,16 +35,17 @@
 
 
 	        //host for students : ldap://ss.wits.ac.za/;ldap://146.141.8.201
-            $ldap_con = ldap_connect("ldap://ss.wits.ac.za/;ldap://146.141.8.201");
+            $ldap_con_student = ldap_connect("ldap://ss.wits.ac.za/;ldap://146.141.8.201");
 
             //host for staff : ldap://ds.wits.ac.za/;ldap://146.141.128.150
-            //$ldap_con = ldap_connect("ldap://ds.wits.ac.za/;ldap://146.141.128.150");
-            ldap_set_option($ldap_con, LDAP_OPT_PROTOCOL_VERSION, 3);
+            $ldap_con_staff = ldap_connect("ldap://ds.wits.ac.za/;ldap://146.141.128.150");
+            ldap_set_option($ldap_con_student, LDAP_OPT_PROTOCOL_VERSION, 3);
+            ldap_set_option($ldap_con_staff, LDAP_OPT_PROTOCOL_VERSION, 3);
 
             // get user
             $token->setToken();
 
-            if ( @ldap_bind($ldap_con,$user_id,$user_password) ||$token->ok) {
+            if ( @ldap_bind($ldap_con_student,$user_id,$user_password) ||$token->ok || @ldap_bind($ldap_con_staff,$user_id,$user_password) ) {
                 // response
 
                 if($token->ok) {
@@ -54,7 +55,7 @@
                     $GLOBALS['user_type'] = $token->user_type;
                     $GLOBALS['user_name'] = $token->user_name;
                     $GLOBALS['user_surname'] = $token->user_surname;
-                }else{
+                }elseif(@ldap_bind($ldap_con_student,$user_id,$user_password)){
                     //this is the insert into db part
                     //instantiate new
 
@@ -65,8 +66,8 @@
                     //Staff Context
                     //$ldapcontext = "ou=wits university,dc=ds,dc=wits,dc=ac,dc=za";
 
-                    $sr=ldap_search($ldap_con, $ldapcontext, $ldapfilter, $ldapjustthese)or die ("Error in search query: ".ldap_error($ldap_con));
-                    $info = ldap_get_entries($ldap_con, $sr);
+                    $sr=ldap_search($ldap_con_student, $ldapcontext, $ldapfilter, $ldapjustthese)or die ("Error in search query: ".ldap_error($ldap_con_student));
+                    $info = ldap_get_entries($ldap_con_student, $sr);
 
                     //Set values
                     $user_name = $info[0]["givenname"][0];
@@ -93,6 +94,41 @@
                     $GLOBALS['user_name'] = $token->user_name;
                     $GLOBALS['user_surname'] = $token->user_surname;
 
+                }else{
+                    //this is the insert into db part
+                    //instantiate new
+
+                    $ldapfilter="(|(cn=".$user_id."))";
+                    $ldapjustthese = array( "sn", "givenName");
+                    //Students Context
+                    //$ldapcontext = "ou=students,ou=wits university,dc=ss,dc=wits,dc=ac,dc=za";
+                    //Staff Context
+                    $ldapcontext = "ou=wits university,dc=ds,dc=wits,dc=ac,dc=za";
+
+                    $sr=ldap_search($ldap_con_staff, $ldapcontext, $ldapfilter, $ldapjustthese)or die ("Error in search query: ".ldap_error($ldap_con_staff));
+                    $info = ldap_get_entries($ldap_con_staff, $sr);
+
+                    //Set values
+                    $user_name = $info[0]["givenname"][0];
+                    $user_surname = $info[0]["sn"][0];
+                    $user_type = 2;
+                    $faculty_name = 5;
+
+                    //Add the user to our db
+                    $connect = mysqli_connect('127.0.0.1','root','','risk');
+                    $sql = 'INSERT INTO user (user_id,user_name,user_surname,user_password,faculty_id,utl_id)
+                            VALUES ('. $user_idt . ', "' . $user_name .
+                        '", "' . $user_surname . '","'.password_hash($user_password,PASSWORD_DEFAULT).'", ' . $faculty_name . ', ' . $user_type . ')';
+                    mysqli_query($connect,$sql);
+                    mysqli_close($connect);
+
+                    // remake token and login
+                    $token->setToken();
+                    http_response_code(200);
+                    $GLOBALS['user_id'] = $token->user_id;
+                    $GLOBALS['user_type'] = $token->user_type;
+                    $GLOBALS['user_name'] = $token->user_name;
+                    $GLOBALS['user_surname'] = $token->user_surname;
                 }
                 // json
                 echo json_encode(array('message' => 'success'));
